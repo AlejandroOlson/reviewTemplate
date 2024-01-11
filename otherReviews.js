eae0eaec7ff8a5e4275050daff116be86750c3b4
Alejandro Olson <alejandroolsonguitar@gmail.com>
2024-01-10 23:58:42 -0500

    Add files via upload

---
+++ otherReviews.js
@@ -0,0 +1,513 @@
+
+let html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Reviews</title>
+  </head>
+  <body>
+    <style>
+      * {
+        box-sizing: border-box;
+      }
+
+      body {
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+        font-size: 14px;
+        line-height: 1.42857143;
+        color: #333;
+        background-color: #fff;
+      }
+
+      div {
+        display: block;
+      }
+
+      .colorbody {
+        background-color: #f4f4f4;
+        font-family: arial, helvetica, sans-serif;
+        font-size: 14px;
+      }
+
+      #tm-771bw div.go3087873059 {
+        padding: 2.5em 0;
+      }
+
+      #tm-771bw h2.go1718788836 {
+        text-align: center;
+        margin-bottom: 1em;
+        color: #000000;
+      }
+
+      #tm-771bw div.go2990454101 {
+        max-width: 50em;
+        margin: 0 auto 1em auto;
+        padding: 0 20px;
+        color: #7b8697;
+        text-align: center;
+      }
+
+      #tm-771bw .go2698594108 .tm-overall {
+        display: none;
+      }
+
+      #tm-771bw .go2402097865,
+      #tm-771bw .go2402097865 *,
+      #tm-771bw .go2402097865 *::before,
+      #tm-771bw .go2402097865 *::after {
+        animation: revert;
+        background: revert;
+        border: revert;
+        position: revert;
+        display: revert;
+        columns: revert;
+        margin: revert;
+        padding: revert;
+        list-style: revert;
+        border-radius: revert;
+        box-shadow: revert;
+        color: revert;
+        font-size: revert;
+        font-style: revert;
+        font-weight: revert;
+        text-align: revert;
+        text-decoration: revert;
+        text-indent: revert;
+        text-shadow: revert;
+        text-transform: revert;
+        transform: revert;
+        transition: revert;
+        vertical-align: revert;
+        word-spacing: revert;
+        word-wrap: revert;
+        word-break: revert;
+        quotes: revert;
+        float: revert;
+        clear: revert;
+        grid: revert;
+        offset: revert;
+        opacity: revert;
+        outline: revert;
+        overflow: revert;
+        height: revert;
+        width: revert;
+        min-height: revert;
+        min-width: revert;
+        max-height: revert;
+        max-width: revert;
+        top: revert;
+        right: revert;
+        bottom: revert;
+        left: revert;
+        line-height: revert;
+        resize: revert;
+        z-index: revert;
+        -webkit-box-decoration-break: revert;
+        -webkit-font-smoothing: revert;
+        -webkit-hyphenate-character: revert;
+        box-sizing: border-box;
+      }
+
+      #tm-771bw div.go3136508558 {
+        overflow: hidden;
+        position: relative;
+        direction: ltr;
+      }
+
+      #tm-771bw .go3864472833 .tm-carousel-outside {
+        /* opacity: 0; */
+      }
+
+      #tm-771bw .go3864472833 .tm-carousel-item {
+        padding: 0.75em;
+        transition: 0.4s opacity ease-in-out;
+      }
+
+      #tm-771bw .go3864472833 .tm-card-wrapper {
+        height: 100%;
+      }
+
+      #tm-771bw div.go2617933356 {
+        display: flex;
+        width: 100%;
+        margin: auto 0;
+      }
+
+      #tm-771bw div.go3834223395 {
+        background: #ffffff;
+        border-radius: 1em;
+        padding: 1.5em;
+        box-shadow: 0 0.75em 1.5em -0.25em #919eab1f, 0 0 0.125em 0 #919eab33;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+        font-size: 1em;
+        line-height: 1.5em;
+        flex: 1;
+        text-align: center;
+      }
+
+      #tm-771bw div.go1874298781 {
+        display: flex;
+        align-items: center;
+        gap: 1em;
+        text-align: center;
+        flex-direction: column;
+      }
+
+      #tm-771bw .go1874298781 .tm-bottom-picture {
+        display: flex;
+        align-items: center;
+      }
+
+      #tm-771bw .go1874298781 .tm-bottom-person {
+        flex: 1;
+        line-height: 1.5em;
+        display: flex;
+        flex-direction: column;
+      }
+
+      #tm-771bw div.go1471129203 {
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+        gap: 1em;
+        justify-content: center;
+      }
+
+      #tm-771bw div.go1502613249 {
+        display: flex;
+        height: 1em;
+        width: 5em;
+        position: relative;
+      }
+
+      #tm-771bw .go1471129203 .tm-time {
+        color: #5f7482;
+      }
+
+      #tm-771bw div.go3073557368 {
+        flex: 1;
+      }
+
+      #tm-771bw .go3018596957 > div {
+        text-align: center;
+      }
+
+      #tm-771bw div.go4008414990 {
+        color: #000;
+      }
+
+      #tm-771bw .go1502613249 > div {
+        /* Change to font awesome star icons! */
+        background-image: url("https://icons8.com/icon/85185/star");
+        width: 24px;
+        height: 24px;
+      }
+
+      #tm-abqkw .go1755512589 a {
+        color: #1d3a6c;
+        text-decoration: underline;
+        font-weight: 600;
+      }
+
+      #tm-abqkw div.go1755512589 {
+        margin-top: 1em;
+      }
+
+      go3087873059 tm-widget {
+        /* width: 70vw; <-- adjust according to the kind of screen!!! */
+      }
+
+      .flex-container {
+        display: flex !important;
+        flex-direction: row;
+      }
+    </style>
+
+    <div>
+      <div id="tm-771bw">
+        <div class="go2402097865">
+          <div class="go3797868838">
+            <div class="go2698594108">
+              <div class="go3087873059 tm-widget">
+                <h2 class="go1718788836 tm-title">
+                  Here is what our customers say
+                </h2>
+                <div class="go3864472833 tm-carousel-wrapper">
+                  <div>
+                    <!-- changed to 0 from 70vw (original 100vw), allows for only 3 cards to show (style="max-width: 70vw" for above <div>)-->
+                    <div class="go3136508558">
+                      <div class="flex-container">
+                        <!-- Derrick review -->
+                        <div class="go3834223395 tm-card">
+                          <div class="go1874298781 tm-card-top">
+                            <div class="tm-bottom-picture">
+                              <svg
+                                viewBox="0 0 40 40"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                style="width: 2.5em; height: 2.5em"
+                              >
+                                <rect
+                                  rx="50"
+                                  fill="#1d3a6c"
+                                  fill-opacity="0.12"
+                                  style="width: 100%; height: 100%"
+                                ></rect>
+                                <text
+                                  x="50%"
+                                  y="54%"
+                                  dominant-baseline="middle"
+                                  text-anchor="middle"
+                                  fill="#1d3a6c"
+                                  style="font-size: 0.875em; font-weight: 600"
+                                >
+                                  D
+                                </text>
+                              </svg>
+                            </div>
+                            <div class="tm-bottom-person">
+                              <b>Derrick</b>
+                            </div>
+                          </div>
+                          <div class="go1471129203 tm-card-info">
+                            <div class="tm-review-stars">
+                              <div class="go1502613249 tm-stars">
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div class="go3073557368 tm-card-review">
+                            <div class="go3018596957 tm-text-review">
+                              <div class="go4008414990 tm-text-review-inner">
+                                really nice place to stay. everyone was very
+                                nice and fun to hang out with. communication was
+                                great and the co-host was there to help you with
+                                anything you needed. would really recommend this
+                                place to all.
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                        <!-- Dennis review -->
+                        <div class="go3834223395 tm-card">
+                          <div class="go1874298781 tm-card-top">
+                            <div class="tm-bottom-picture">
+                              <svg
+                                viewBox="0 0 40 40"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                style="width: 2.5em; height: 2.5em"
+                              >
+                                <rect
+                                  rx="50"
+                                  fill="#1d3a6c"
+                                  fill-opacity="0.12"
+                                  style="width: 100%; height: 100%"
+                                ></rect>
+                                <text
+                                  x="50%"
+                                  y="54%"
+                                  dominant-baseline="middle"
+                                  text-anchor="middle"
+                                  fill="#1d3a6c"
+                                  style="font-size: 0.875em; font-weight: 600"
+                                >
+                                  D
+                                </text>
+                              </svg>
+                            </div>
+                            <div class="tm-bottom-person">
+                              <b>Dennis</b>
+                            </div>
+                          </div>
+                          <div class="go1471129203 tm-card-info">
+                            <div class="tm-review-stars">
+                              <div class="go1502613249 tm-stars">
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div class="go3073557368 tm-card-review">
+                            <div class="go3018596957 tm-text-review">
+                              <div class="go4008414990 tm-text-review-inner">
+                                It was late when i finaly booked and didnt look
+                                promising that id get a reservation but
+                                northstar called me personally to accommodate my
+                                Late-night dollar-short request, they go extra
+                                miles
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                        <!-- Nikki review -->
+                        <div class="go3834223395 tm-card">
+                          <div class="go1874298781 tm-card-top">
+                            <div class="tm-bottom-picture">
+                              <svg
+                                viewBox="0 0 40 40"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                style="width: 2.5em; height: 2.5em"
+                              >
+                                <rect
+                                  rx="50"
+                                  fill="#1d3a6c"
+                                  fill-opacity="0.12"
+                                  style="width: 100%; height: 100%"
+                                ></rect>
+                                <text
+                                  x="50%"
+                                  y="54%"
+                                  dominant-baseline="middle"
+                                  text-anchor="middle"
+                                  fill="#1d3a6c"
+                                  style="font-size: 0.875em; font-weight: 600"
+                                >
+                                  N
+                                </text>
+                              </svg>
+                            </div>
+                            <div class="tm-bottom-person">
+                              <b>Nikki</b>
+                            </div>
+                          </div>
+                          <div class="go1471129203 tm-card-info">
+                            <div class="tm-review-stars">
+                              <div class="go1502613249 tm-stars">
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                                <img
+                                  width="24"
+                                  height="24"
+                                  src="https://img.icons8.com/material-rounded/24/FAB005/star--v1.png"
+                                  alt="star--v1"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div class="go3073557368 tm-card-review">
+                            <div class="go3018596957 tm-text-review">
+                              <div class="go4008414990 tm-text-review-inner">
+                                I truly enjoyed my stay. This place is as
+                                advertised. I even enjoyed the nostalgic feeling
+                                of being away at college. Thank you for the
+                                accommodations.
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+
+`
+
+
+function loadReviews() {
+
+    const reviewBox = document.getElementById("review-div");
+
+    reviewBox.insertAdjacentHTML(
+        "afterbegin",
+        html
+    )
+}
+
+loadReviews();
